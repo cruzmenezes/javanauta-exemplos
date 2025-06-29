9173039eec772059985771d6edadc00c3d6aba72
cruzmenezes <cruz.menezes@outlook.com>
2025-06-28 23:22:13 -0300

    aula de logica

---
+++ javanauta/src/main/java/org/example/logica/ExpressoesComparativas.java
@@ -0,0 +1,17 @@
+package org.example.logica;
+
+public class ExpressoesComparativas {
+
+    public static void main(String[] args) {
+
+        int pao = 20;
+        int acucar = 10;
+
+        System.out.println(pao == acucar);
+        System.out.println(pao != acucar);
+        System.out.println(pao < acucar);
+        System.out.println(pao > acucar);
+        System.out.println(pao <= acucar);
+        System.out.println(pao >= acucar);
+    }
+}
---
+++ javanauta/src/main/java/org/example/logica/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package org.example.logica;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+        System.out.println(x || y);
+        System.out.println(!x);
+    }
+}
