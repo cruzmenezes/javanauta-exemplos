dead6e1de9886a98588b6ae31aa75e169b1f0175
cruzmenezes <cruz.menezes@outlook.com>
2025-07-01 18:30:06 -0300

    conversão de inteiro para caracter

---
+++ javanauta/src/main/java/org/example/exemplosdiversos/ConversaoInteiroParaCaracteres.java
@@ -0,0 +1,19 @@
+package org.example.exemplosdiversos;
+
+public class ConversaoInteiroParaCaracteres {
+
+    public static void main(String[] args) {
+
+        int num1 = 79;
+        int num2 = 108;
+        int num3 = 225;
+
+        char letra1 = (char) num1;
+        char letra2 = (char) num2;
+        char letra3 = (char) num3;
+
+        System.out.println("Saudação: " + letra1 + letra2 + letra3);
+
+
+    }
+}
